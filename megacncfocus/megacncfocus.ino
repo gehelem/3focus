3db95d94b50e339effbc67d89519f8b335e4b63b
gehelem <gilles@joag.fr>
2017-12-31 17:30:25 +0100

    new version

--- megacncfocus/megacncfocus.ino
+++ megacncfocus/megacncfocus.ino
@@ -1,5 +1,7 @@
 #define STEPONTIMEuS 5
 #define ENABLETIMEuS 500
+#define DEBOUNCEmS 200
+#define ENCODERINC 10
 #define MyDirX      5
 #define MyStepX     2
 #define MyDirY      6
@@ -7,17 +9,19 @@
 #define MyDirZ      7
 #define MyStepZ     4
 #define MyEnable    8
-#define encoder0PinS    14
-#define encoder0PinA    18
-#define encoder0PinB    19
+#define encoderPinS    14
+#define encoderPinA    18
+#define encoderPinB    19
 #define ledXpin   A8
 #define ledYpin   A9
 #define ledZpin   A10
 
 int btnpos = 0;
-int actualposition = 0;
-int targetposition = 0;
-int changeposition = 0;
+volatile int actualposition = 0;
+volatile int targetposition = 0;
+volatile int encoderposition = 0;
+boolean A_set = false;
+boolean B_set = false;
 
 void setup() {
   
@@ -31,12 +35,12 @@ void setup() {
   pinMode(ledYpin, OUTPUT);
   pinMode(ledZpin, OUTPUT);  
   pinMode(MyEnable, OUTPUT);
-  pinMode(encoder0PinA, INPUT);
-  pinMode(encoder0PinB, INPUT);
-  pinMode(encoder0PinS, INPUT);  
+  pinMode(encoderPinA, INPUT);
+  pinMode(encoderPinB, INPUT);
+  pinMode(encoderPinS, INPUT);  
 
-  attachInterrupt(digitalPinToInterrupt(encoder0PinA), doEncoderA, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(encoder0PinB), doEncoderB, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoderPinA), doEncoderA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoderPinB), doEncoderB, CHANGE);
   
   digitalWrite(MyDirX, LOW);
   digitalWrite(MyStepX, LOW);
@@ -50,12 +54,50 @@ void setup() {
   digitalWrite(ledYpin, HIGH);
   digitalWrite(ledZpin, HIGH);
 
+  Serial.begin (9600);
+
+
 }
 
 void loop() {
+  if (encoderposition != actualposition) {
+    Serial.println (encoderposition, DEC);  
+    targetposition = encoderposition;
+    dosteps();
+  }    
+
+  if (digitalRead(encoderPinS) == LOW) {
+    btnpos++;
+    if (btnpos > 3) btnpos = 0;
+    digitalWrite(ledXpin, LOW);
+    digitalWrite(ledYpin, LOW);
+    digitalWrite(ledZpin, LOW);
+    if ((btnpos==0) || (btnpos == 1)) digitalWrite(ledXpin, HIGH);
+    if ((btnpos==0) || (btnpos == 2)) digitalWrite(ledYpin, HIGH);
+    if ((btnpos==0) || (btnpos == 3)) digitalWrite(ledZpin, HIGH);
+    delay(DEBOUNCEmS);    
+  }
   
-  if (changeposition != actualposition) {
-    targetposition = changeposition;
+  
+}
+
+void doEncoderA() {
+  delay(DEBOUNCEmS);
+  if ( digitalRead(encoderPinA) != A_set ) {
+    A_set = !A_set;
+    if ( A_set && !B_set ) encoderposition += ENCODERINC;
+  }
+}
+
+void doEncoderB() {
+  delay(DEBOUNCEmS);
+  if ( digitalRead(encoderPinB) != B_set ) {
+    B_set = !B_set;
+    if ( B_set && !A_set ) encoderposition -= ENCODERINC;
+  }
+}
+
+void dosteps() {
     if (targetposition > actualposition) {
           digitalWrite(MyDirX, HIGH);
           digitalWrite(MyDirY, HIGH);
@@ -74,66 +116,9 @@ void loop() {
           for (;actualposition>targetposition;actualposition--) dostep(); 
           digitalWrite(MyEnable, HIGH);   
     } 
-  }
-
 }
 
-void doEncoderA() {
-  // look for a low-to-high on channel A
-  if (digitalRead(encoder0PinA) == HIGH) {
-
-    // check channel B to see which way encoder is turning
-    if (digitalRead(encoder0PinB) == LOW) {
-          changeposition++;
-    }
-    else {
-          changeposition--;
-    }
-  }
-
-  else   // must be a high-to-low edge on channel A
-  {
-    // check channel B to see which way encoder is turning
-    if (digitalRead(encoder0PinB) == HIGH) {
-          changeposition++;
-    }
-    else {
-          changeposition--;
-    }
-  }
-
-}
-
-void doEncoderB() {
-
-  // look for a low-to-high on channel B
-  if (digitalRead(encoder0PinB) == HIGH) {
-
-    // check channel A to see which way encoder is turning
-    if (digitalRead(encoder0PinA) == HIGH) {
-          changeposition++;
-    }
-    else {
-          changeposition--;
-    }
-  }
-
-  // Look for a high-to-low on channel B
-
-  else {
-    // check channel B to see which way encoder is turning
-    if (digitalRead(encoder0PinA) == LOW) {
-          changeposition++;
-    }
-    else {
-          changeposition--;
-    }
-  }
-
-}
-
-
-void dostep () {
+void dostep() {
       if ((btnpos==0) || (btnpos == 1)) digitalWrite(MyStepX, HIGH);
       if ((btnpos==0) || (btnpos == 2)) digitalWrite(MyStepY, HIGH);
       if ((btnpos==0) || (btnpos == 3)) digitalWrite(MyStepZ, HIGH);
@@ -141,7 +126,8 @@ void dostep () {
       if ((btnpos==0) || (btnpos == 1)) digitalWrite(MyStepX, LOW);
       if ((btnpos==0) || (btnpos == 2)) digitalWrite(MyStepY, LOW);
       if ((btnpos==0) || (btnpos == 3)) digitalWrite(MyStepZ, LOW);
-      delayMicroseconds(STEPONTIMEuS);   
+      delayMicroseconds(STEPONTIMEuS*200);   
   
 }
+
 
